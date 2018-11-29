79873fb3898eb301b249ee392d09f342529ec50c
Tim-Oliver Husser <thusser@uni-goettingen.de>
2018-11-29 09:46:46 +0100

    fixed setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description='spexxy spectrum fitting package',
     author='Tim-Oliver Husser',
     author_email='thusser@uni-goettingen.de',
-    packages=find_packages('.', include=['spexxy']),
+    packages=find_packages(include=['spexxy', 'spexxy.*']),
     scripts=['bin/spexxy'],
     include_package_data=True,
     requires=['scipy', 'numpy', 'astropy', 'pandas', 'lmfit']
