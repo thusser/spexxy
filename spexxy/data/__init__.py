ececb48903a03c6ba3843285281ef4ef1e299ab3
Tim-Oliver Husser <thusser@uni-goettingen.de>
2018-12-07 13:37:49 +0100

    added missing import

--- spexxy/data/__init__.py
+++ spexxy/data/__init__.py
@@ -1,5 +1,6 @@
 from .filter import Filter
 from .fitsspectrum import FitsSpectrum
+from .isochrone import Isochrone
 from .losvd import LOSVD
 from .lsf import LSF, AnalyticalLSF, EmpiricalLSF
 from .resultsfits import ResultsFITS
