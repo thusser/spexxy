f6470a07b1e7f02f3f951a81ad145d83726e4d35
Tim-Oliver Husser <thusser@uni-goettingen.de>
2019-03-25 15:36:17 +0100

    added "lsf apply" command to tools

---
+++ spexxy/tools/lsf/apply.py
@@ -0,0 +1,26 @@
+from spexxy.data.lsf import LSF
+from spexxy.data.spectrum import Spectrum
+
+
+def add_parser(subparsers):
+    # apply
+    parser = subparsers.add_parser('apply')
+    parser.set_defaults(func=run)
+    parser.add_argument('lsf', metavar='lsf', type=str, help='LSF to convolve with')
+    parser.add_argument('spectrum', metavar='spectrum', type=str, help='spectrum to convolve')
+    parser.add_argument('output', metavar='output', type=str, help='output file')
+
+
+def run(args):
+    # load LSF and spec
+    lsf = LSF(args.lsf)
+    spec = Spectrum.load(args.spectrum)
+
+    # scale
+    lsf.resample(spec)
+
+    # apply
+    output = lsf.apply(spec)
+
+    # save it
+    output.save(args.output)
