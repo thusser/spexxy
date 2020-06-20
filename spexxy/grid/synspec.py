b482f515a4b6d3d52b061ac6e83462153cd33011
Tim-Oliver Husser <thusser@uni-goettingen.de>
2020-06-20 21:23:27 +0200

    working version with way too many input variables

--- spexxy/grid/synspec.py
+++ spexxy/grid/synspec.py
@@ -1,3 +1,5 @@
+import shutil
+from tempfile import mkdtemp
 from typing import List, Any, Tuple
 import pandas as pd
 import os
@@ -10,7 +12,7 @@ class SynspecGrid(Grid):
     """Synthesizes a new spectrum with Synspec at given grid positions."""
 
     def __init__(self, models: Grid, vturbs: Grid, synspec: str, inpmol: str, linelist: str, mollist: str, iat: float,
-                 el_sol: float, *args, **kwargs):
+                 el_sol: float, datadir: str, gfmol: str, nstmol: str, inputfiles: str, *args, **kwargs):
         """Constructs a new Grid.
 
         Args:
@@ -22,6 +24,10 @@ class SynspecGrid(Grid):
             mollist: File with molecular list
             iat: bla
             el_sol: bla
+            datadir: Name of data directory
+            gfmol: bla
+            nstmol: bla
+            inputfiles: bla
         """
         Grid.__init__(self, axes=None, *args, **kwargs)
 
@@ -32,6 +38,10 @@ class SynspecGrid(Grid):
         self._mollist = mollist
         self._iat = iat
         self._el_sol = el_sol
+        self._datadir = datadir
+        self._gfmol = gfmol
+        self._nstmol = nstmol
+        self._inputfiles = inputfiles
 
         # load grids
         self._models: Grid = self.get_objects(models, Grid, 'grids', self.log, single=True)
@@ -81,12 +91,92 @@ class SynspecGrid(Grid):
             Grid value at given position.
         """
         print(params)
+        # get params
+        teff, logg, feh, alpha, el = params
 
         # find element in models grid
         model_params = self._models.nearest(params[:-1])
-        model = self._models.filename(model_params)
-        print(model_params, model)
+        mod = self._models.filename(model_params)
+        print(model_params, mod)
 
+        # temp directory
+        tmp = os.path.abspath(mkdtemp(dir='tmp'))
+
+        # init
+        subgrid = 'Z-1.0'
+        self._make_inp5(tmp, teff, logg, subgrid)
+        self._make_nst(tmp)
+
+        self._make_inp56(tmp, self._iat, self._el_sol, el)
+        self._copy_inputs(tmp, mod, self._inpmol, self._linelist, self._mollist)
+
+        os.symlink(self._synspec, os.path.join(tmp, 'synspec'))
+
+        cwd = os.getcwd()
+        os.chdir(tmp)
+        os.system('pwd')
+        os.system('./synspec < fort.5 > fort.6')
+        d = pd.read_csv('fort.7', delim_whitespace=True, names=['wave', 'flux'])
+        os.chdir(cwd)
+
+        spec = Spectrum(wave=d['wave'], flux=d['flux'])
+        print(spec.wave_start, spec.wave_step)
+
+        shutil.rmtree(tmp)
+        return spec
+
+    def _make_inp5(self, tmp, teff, logg, subgrid):
+        ## read a template for the inp.5 file
+        ## and replace teff and grav by the appropriate values
+
+        with open(os.path.join(self._inputfiles, 'inp.5' + subgrid.strip('/')), 'r') as f:
+            inp5temp = f.read()
+
+        inp5 = inp5temp.replace('$TEFF', str(teff)).replace('$GRAV', str(logg))
+
+        with open(os.path.join(tmp, 'fort.5'), 'w') as f:
+            f.write(inp5)
+
+    def _make_nst(self, tmp):
+        ## I go open and read the original phoenix spectra, to retrieve information on the turbulent
+        ## velocity of the original model atmosphere, which is contained in the header
+        #pathf = 'lte04800-1.50-1.0.PHOENIX-ACES-AGSS-COND-2011-HiResMuse.fits'
+        #hdu = fits.open(pathf)
+        #vtb_phoenix = hdu[0].header['PHXXI_L']
+
+        ## I will update the template nst file and put the right VTB value in there
+
+        with open(self._nstmol, 'r') as f:
+            temp = f.read()
+
+        #nstnew = temp.replace('$VTB', str(vtb_phoenix))
+        nstnew = temp.replace('$VTB', str(2))
+
+        with open(os.path.join(tmp, 'nstphoenix'), 'w') as f:
+            f.write(nstnew)
+
+    def _make_inp56(self, tmp, iat, ab_sol, abund_up):
+        # To create the fort.56 that contains the updated
+        # abundance (abund_up) for the element wanted
+        # All we need is a file with :
+        # -----
+        # 1
+        # iat abund_up
+
+        abund_elem = ab_sol * 10 ** abund_up
+
+        with open(os.path.join(tmp, "fort.56"), 'w') as f:
+            f.write('1 \n')
+            f.write('%i  %0.3e \n' % (iat, abund_elem))
+
+    def _copy_inputs(self, tmp, mod, inp55, llist, listmol):
+        shutil.copy(mod, os.path.join(tmp, 'fort.8'))  # model atmosphere into fort.8
+        shutil.copy(inp55, os.path.join(tmp, 'fort.55'))  # the standard input for synspec
+
+        os.symlink(os.path.abspath(llist), os.path.join(tmp, 'fort.19'))
+        os.symlink(os.path.abspath(listmol), os.path.join(tmp, 'fort.26'))
+        os.symlink(self._gfmol, os.path.join(tmp, 'fort.27'))
+        os.symlink(self._datadir, os.path.join(tmp, 'data'))
 
 
 __all__ = ['SynspecGrid']
