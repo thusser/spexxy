15059f9ea4e4c2b18a857c881bd0641e9b6b6feb
Tim-Oliver Husser <thusser@uni-goettingen.de>
2020-06-23 17:00:44 +0200

    added option to show all values

--- spexxy/tools/grid/info.py
+++ spexxy/tools/grid/info.py
@@ -18,12 +18,20 @@ def add_parser(subparsers):
     parser = subparsers.add_parser('info', help='Prints information about a given grid',
                                    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('config', type=str, help='Config for grid')
+    parser.add_argument('-v', '--values', action='store_true', help='Show all values on axes')
 
     # argparse wrapper for create_grid
     parser.set_defaults(func=lambda args: get_spectrum_from_grid(**vars(args)))
 
 
-def get_spectrum_from_grid(config: str, **kwargs):
+def get_spectrum_from_grid(config: str, values: bool, **kwargs):
+    """Prints info about grid given in config.
+
+    Args:
+        config: Config to build grid from.
+        values: If True, print all values per axis.
+    """
+
     # create grid
     with open(config, 'r') as f:
         cfg = yaml.safe_load(f)
@@ -42,7 +50,9 @@ def get_spectrum_from_grid(config: str, **kwargs):
 
         # values?
         if ax.values is not None:
-            print('     # steps:  %d' % len(ax.values))
+            print('     # steps: %d' % len(ax.values))
+            if values:
+                print('     values:  %s' % ','.join(['%.2f' % v for v in ax.values]))
         else:
             print('     continuous')
 
