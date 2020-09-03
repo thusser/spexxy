2f4cb457ea9c6ac9cd64143f12725dacbf515cfd
Tim-Oliver Husser <thusser@uni-goettingen.de>
2020-09-03 19:28:17 +0200

    added losvd parameter which can skip the losvd parameters

--- spexxy/component/spectrumcomponent.py
+++ spexxy/component/spectrumcomponent.py
@@ -9,14 +9,15 @@ from ..object import create_object
 class SpectrumComponent(Component):
     """SpectrumComponent is the base Component class for all components that deal with spectra."""
 
-    def __init__(self, name: str, model_func: Callable, losvd_hermite: bool = False, vac_to_air: bool = False,
-                 lsf: Union[LSF, str, dict] = None, *args, **kwargs):
+    def __init__(self, name: str, model_func: Callable, losvd: bool = True, losvd_hermite: bool = False,
+                 vac_to_air: bool = False, lsf: Union[LSF, str, dict] = None, *args, **kwargs):
         """Initializes a new SpectrumComponent.
 
         Args:
             name: Name of new component
             model_func: Method that returns a spectrum for the given parameters,
                         should be implemented by derived classes
+            losvd: Whether to add losvd parameters
             losvd_hermite: Whether or not Hermite polynomials should be used for the LOSVD
             vac_to_air: If True, vac_to_air() is called on spectra returned from the model_func
             lsf: LSF to apply to spectrum
@@ -24,16 +25,18 @@ class SpectrumComponent(Component):
         Component.__init__(self, name, *args, **kwargs)
         self._vac_to_air = vac_to_air
         self._model_func = model_func
+        self._losvd = losvd
         self._losvd_hermite = losvd_hermite
 
         # add losvd parameters
-        self.set('v', min=-2000., max=2000., value=1.)
-        self.set('sig', min=0., max=500., value=10.)
-        if self._losvd_hermite:
-            self.set('h3', min=-0.3, max=0.3, value=0.)
-            self.set('h4', min=-0.3, max=0.3, value=0.)
-            self.set('h5', min=-0.3, max=0.3, value=0.)
-            self.set('h6', min=-0.3, max=0.3, value=0.)
+        if losvd:
+            self.set('v', min=-2000., max=2000., value=1.)
+            self.set('sig', min=0., max=500., value=10.)
+            if self._losvd_hermite:
+                self.set('h3', min=-0.3, max=0.3, value=0.)
+                self.set('h4', min=-0.3, max=0.3, value=0.)
+                self.set('h5', min=-0.3, max=0.3, value=0.)
+                self.set('h6', min=-0.3, max=0.3, value=0.)
 
         # get lsf
         if isinstance(lsf, LSF):
@@ -60,20 +63,22 @@ class SpectrumComponent(Component):
             if key in self.parameters:
                 self.set(key, value=val)
 
-        # get LOSVD parameters
-        losvd_params = ['v', 'sig'] + (['h3', 'h4', 'h5', 'h6'] if self._losvd_hermite else [])
-        losvd = [self[p] for p in losvd_params]
-
         # get  model
         model = self._model_func()
 
-        # apply LSF
-        if self._lsf is not None:
-            self._lsf.resample(model)
-            model = self._lsf(model)
+        # losvd?
+        if self._losvd:
+            # get LOSVD parameters
+            losvd_params = ['v', 'sig'] + (['h3', 'h4', 'h5', 'h6'] if self._losvd_hermite else [])
+            losvd = [self[p] for p in losvd_params]
 
-        # apply losvd
-        self._apply_losvd(model, losvd)
+            # apply LSF
+            if self._lsf is not None:
+                self._lsf.resample(model)
+                model = self._lsf(model)
+
+            # apply losvd
+            self._apply_losvd(model, losvd)
 
         # vac2air
         if self._vac_to_air:
